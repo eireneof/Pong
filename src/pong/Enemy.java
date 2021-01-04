8ca3beb36c0674f8960bb8b6ea7984cf7ace727b
Eirene Fireman <eof@ic.ufal.br>
2021-01-04 14:10:33 -0300

    arquivos iniciais

---
+++ src/pong/Enemy.java
@@ -0,0 +1,26 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Enemy {
+	
+	public double x, y;
+	public int widht, height;
+	
+	public Enemy(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.widht = 40;
+		this.height = 5;
+	}
+
+	public void tick() {
+		x += (Game.ball.x - x - 6) * 0.3;
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.red);
+		g.fillRect((int)x, (int)y, widht, height);
+	}
+}
